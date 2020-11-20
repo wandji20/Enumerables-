c2c1132930055ea8d86d48d2f5eb7f362643b8cf
wandji20 <wbertrand10@gmail.com>
2020-11-20 15:22:28 +0100

    Make methods for my_count and my_map

--- main.rb
+++ main.rb
@@ -52,11 +52,49 @@ module Enumerable
     return isNone
   end
   #my_none?
+
+  #my_count
+  def my_count
+    index=0
+    for item in self
+      index+=1
+    end
+    index
+  end
+  #my_none
+
+  #my_map
+  def my_map
+    new_arr=[]
+    for item in self
+      transformed=yield(item)
+      new_arr.push(transformed)
+    end
+    new_arr
+  end   
+  #my_map
+
+  #my_inject
+  def my_inject(sign)
+    result = 0 if (sign == +) || (sign == -)
+    result = 1 if (sign == *) || (sign == //)
+    for item in self
+     result = sign=item
+    end
+    result
+  end
+  #my_inject
+
+ 
 end
 
+
 test_array = [1, 2, 3, 4, 5]
+p test_array.my_inject(:+)
 # test_array.my_each {|item| p item }
 # test_array.my_each_with_index { |item, index| p item, index }
 # test_array.my_select {|num| num.even? }
 # test_array.my_all? {|num| num.even? }
-p test_array.my_none? {|num| num > 5 }
+# test_array.my_none? {|num| num > 5 }
+# test_array.my_count
+# test_array.my_map{|item| item+2}
