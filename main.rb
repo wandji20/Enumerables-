cb01c2abf2b21eca7853e674f64c68088e26f0d1
rashad <omar_rashad@aol.com>
2020-11-24 13:04:39 +0200

    Fixing local linter errors

--- main.rb
+++ main.rb
@@ -19,7 +19,7 @@ module Enumerable
     return to_enum(:my_each_with_index) unless block_given?
 
     index = 0
-    each do |item|
+    my_each do |item|
       yield(item, index) if block_given?
       index += 1
     end
@@ -30,148 +30,83 @@ module Enumerable
     return to_enum(:my_select) unless block_given?
 
     new_arr = []
-    each do |item|
-      new_arr << item if yield(item)
-    end
+    my_each { |item| new_arr << item if yield(item) }
     new_arr
   end
 
   # my_all?
-  def my_all?(arg = nil)
+  def my_all?(arg = nil, &_block)
     flag = true
     case arg
-    when arg.instance_of?(Class)
-      each { |item| flag = false unless item.is_a? arg }
-      return flag
-    when arg.instance_of?(Regexp)
-      each { |item| flag = false if arg.match(item.to_s).nil? }
-      return flag
-    when arg.instance_of?(String)
-      each {|item| flag = false unless item.include? arg}
-      return flag
-    when block_given?
-      each do |item|
-        feedback = yield(item)
-        flag = false if (feedback == false) || feedback.nil?
+    when Class
+      my_each { |item| flag = false unless item.is_a? arg }
+    when Regexp
+      my_each { |item| flag = false if arg.match(item.to_s).nil? }
+    when String
+      my_each { |item| flag = false unless item.include? arg }
+    when nil
+      unless block_given?
+        arg = proc { |obj| obj }
+        my_each { |item| flag = false if (arg.call(item) == false) || arg.call(item).nil? }
+        return flag
       end
+      my_each { |item| flag = false if (yield(item) == false) || yield(item).nil? }
       return flag
-    when !(block_given?)
-      arg = proc { |obj| obj }
-      each do |item|
-        feedback = arg.call(item)
-        flag = false if (feedback == false) || feedback.nil?
-      end
-      flag
     end
-  
-    #if arg.instance_of?(Class)
-    #  each { |item| flag = false unless item.is_a? arg }
-    #  return flag
-    #elsif arg.instance_of?(Regexp)
-    #  each { |item| flag = false if arg.match(item.to_s).#nil? }
-    #  return flag
-    #elsif arg.instance_of?(String)
-    #  each {|item| flag = false unless item.include? arg}
-    #  return flag
-    #elsif block_given?
-    #  each do |item|
-    #    feedback = yield(item)
-    #    flag = false if (feedback == false) || feedback.nil?
-    #  end
-    #  return flag
-    #end
-    #unless block_given?
-    #  arg = proc { |obj| obj }
-    #  each do |item|
-    #    feedback = arg.call(item)
-    #    flag = false if (feedback == false) || feedback.nil?
-    #  end
-    #  flag
-    #end
+    flag
   end
 
   # my_any?
   def my_any?(arg = nil)
     flag = false
-    if arg.instance_of?(Class)
-      each do |item|
-        flag = true if item.is_a? arg
-      end
-      return flag
-    elsif arg.instance_of?(Regexp)
-      each do |item|
-        flag = true unless arg.match(item.to_s).nil?
-      end
-      return flag
-    elsif arg.instance_of?(String)
-      each do |item|
-        flag = true if item.to_s.include? arg
-      end
-      return flag
-    elsif block_given?
-      each do |item|
-        flag = true if yield(item)
-      end
-      return flag
+    case arg
+    when Class
+      my_each { |item| flag = true if item.is_a? arg }
+    when Regexp
+      p 'here1'
+      my_each { |item| flag = true if arg.match(item.to_s) }
+    when String
+      my_each { |item| flag = true if item.to_s.include? arg }
     end
-    unless block_given?
+    if block_given? && arg.nil?
+      my_each { |item| flag = true if yield(item) }
+    elsif !block_given? && arg.nil?
       flag = true
       arg = proc { |obj| obj }
-      each do |item|
-        flag = false if (arg.call(item) == false) || arg.call(item).nil?
-      end
-      flag
+      my_each { |item| flag = false if (arg.call(item) == false) || arg.call(item).nil? }
     end
+    flag
   end
 
   # my_none?
   def my_none?(arg = nil)
     flag = true
-    if arg.instance_of?(Class)
-      each do |item|
-        flag = false if item.is_a? arg
-      end
-      return flag
-    elsif arg.instance_of?(Regexp)
-      each do |item|
-        flag = false if arg.match(item)
-      end
-      return flag
-    elsif arg.instance_of?(String)
-      each do |item|
-        flag = false if item.to_s.include? arg
-      end
-      return flag
-    elsif block_given?
-      each do |item|
-        flag = true if yield(item)
-      end
-      return flag
+    case arg
+    when Class
+      my_each { |item| flag = false if item.is_a? arg }
+    when Regexp
+      my_each { |item| flag = false if arg.match(item) }
+    when String
+      my_each { |item| flag = false if item.to_s.include? arg }
     end
-    unless block_given?
+    if block_given? && arg.nil?
+      my_each { |item| flag = true if yield(item) }
+    elsif !block_given? && arg.nil?
       arg = proc { |obj| obj }
-      each do |item|
-        flag = false if arg.call(item)
-      end
-      flag
+      my_each { |item| flag = false if arg.call(item) }
     end
+    flag
   end
 
   # my_count
   def my_count(arg = nil)
     counter = 0
     if arg
-      each do |item|
-        counter += 1 if item == arg
-      end
+      my_each { |item| counter += 1 if item == arg }
     elsif block_given?
-      each do |item|
-        counter += 1 if yield(item)
-      end
+      my_each { |item| counter += 1 if yield(item) }
     else
-      each do
-        counter += 1
-      end
+      my_each { counter += 1 }
     end
     counter
   end
@@ -182,14 +117,14 @@ module Enumerable
 
     new_arr = []
     if proc.is_a? Proc
-      each do |item|
+      my_each do |item|
         new_item = proc.call(item)
         new_arr << new_item
       end
       return new_arr
     end
 
-    each do |item|
+    my_each do |item|
       new_item = block.call(item)
       new_arr << new_item
     end
@@ -202,26 +137,22 @@ module Enumerable
 
     if init && sign
       result = init
-      each do |item|
+      my_each do |item|
         result = result.send(sign, item)
       end
     elsif init.is_a?(Symbol) && sign.nil?
       result = first
-      each do |item|
+      my_each do |item|
         result = result.send(init, item)
       end
     elsif block_given?
       result = init || first
-      for i in (1..length-1) 
+      (1..length - 1).each do |i|
         result = yield(result, self[i])
       end
-      #each do |item|
-        #result = yield(result, item)
-      #end
-      
     elsif my_all?(String)
       longst_word = 0
-      each do |item|
+      my_each do |item|
         longst_word = item.length if item.length > longst_word
       end
       return longst_word
@@ -244,7 +175,7 @@ end
 ###  ###  ####  ###
 
 # test_array = [1, 2, 3,5]
-# test_array = ['abc','defg','hijk','lmnop']
+test_array = %w[a21bc de21fg hi12jk lm12nop]
 # test_array = ['abc', 123, 'hijk', 'lmnop']
 # my_proc = Proc.new { |x| x * 10 }
 
@@ -253,14 +184,12 @@ end
 # p test_array.my_each
 # p test_array.my_each_with_index { |item, index| p item, index }
 # p test_array.my_select {|num| num.even? }
-# p test_array.my_all? (/2/)
+p test_array.my_all? { |ele| ele.length < 100 }
 # p test_array.my_any?(/0/)
-# p test_array.my_none?(/z/)
+# p test_array.my_none?(/a/)
 # p test_array.my_count { |ele| ele.is_a? String }
 # p test_array.my_map { |x| x * 100 }
 # p test_array.my_map(my_proc) { |x| puts x * 100 }
-# test_array.my_inject { |sum, n| sum * n }
+# p test_array.my_inject { |sum, n| sum * n }
 # p multiply_els(test_array)
 # test_array.inject { |sum, n| sum + n }
-
-
